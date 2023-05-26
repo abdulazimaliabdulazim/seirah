fe15ff197f7a27d4169c3273e6bedd75e9c68fe1
abdo <abdoAligmail.com>
2023-05-26 06:57:23 +0300

    mody

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './components/APP';
 
 
 ReactDom.render(
-  <div>Hello React</div>,
+  <App />,
   document.getElementById("root")
 )
-
-
-console.log("Hello React");
